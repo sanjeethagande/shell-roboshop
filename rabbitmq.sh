e96085c7269ef40cc00d88e697673fc7cbbdfe1a
Sanjeetha <sanjeetha.gande@gmail.com>
2026-02-05 11:07:31 +1100

    roboshop

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+
+SCRIPT_DIR=$PWD
 source ./common.sh
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added rabbitmq server"
+
+dnf install rabbitmq-server -y &>>LOGS_FILE
+VALIDATE $? "installing rabbitMQ server"
+
+systemctl enable rabbitmq-server
+VALIDATE $? "Enabling rabbitMQ server"
+
+systemctl start rabbitmq-server
+VALIDATE $? "Starting rabbitMQ server"
+
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "created User and gein permissions"
+
